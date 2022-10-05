d42557ec00e5cfce8290294acaec6db1af8b97d9
GianmarcoSanzone <sanzonegianmarco@gmail.com>
2022-10-04 21:26:19 -0400

    EnemyAI updated and GameManager created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+	public GameObject player;
+	public playerController playerScript;
+    // Start is called before the first frame update
+	void Awake()
+    {
+	    instance = this;
+	    player = GameObject.FindGameObjectWithTag("Player");
+	    playerScript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/enemyAi.cs
+++ Assets/Scripts/enemyAi.cs
@@ -1,28 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class enemyAi : MonoBehaviour, IDamage
 {
 	//health bars/health for enemy
 	public float maxHealth = 10;
 	public float currentHealth;
-	
 	public HealthBar healthBar;
 	
-	[SerializeField] int speed;
-	private bool dirRight = true;
-	public Transform target;
+	
+	
 	private bool InRadius;
-	Rigidbody rig;
-	Vector2 move;
+	[SerializeField] NavMeshAgent agent;
+
+	
 	
 	
 	
 	// Start is called before the first frame update
 	void Start()
 	{
-		rig = GetComponent<Rigidbody>();
+		
 		currentHealth = maxHealth;
 		healthBar.SetMaxHealth(maxHealth);
 	}
@@ -42,40 +41,17 @@ public class enemyAi : MonoBehaviour, IDamage
 
 	void Aggro()
 	{
-		Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
-		rig.MovePosition(pos);
-		transform.LookAt(target);
+		
+		agent.SetDestination(GameManager.instance.player.transform.position);
+		
+		
+		//Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+		//rig.MovePosition(pos);
+		//transform.LookAt(target);
 	}
 
 
-	// Update is called once per frame
-	//void Docile()
-	//{
 
-
-	//Move the enemy back and forth
-	//if (dirRight)
-	//{
-	//    transform.Translate(Vector2.right * speed * Time.deltaTime);
-	//}
-
-	//else
-	//{
-	//    transform.Translate(-Vector2.right * speed * Time.deltaTime);
-	//}
-
-	//if (transform.position.x >= 6.0f)
-	//{
-	//    dirRight = false;
-	//}
-
-	//if (transform.position.x <= -6.0f)
-	//{
-	//    dirRight = true;
-	//}
-
-
-	//}
 
 
 	public void takeDamage(float dmg)
