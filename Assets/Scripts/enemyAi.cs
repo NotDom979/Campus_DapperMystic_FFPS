27e30a22b830999de46e7ed75d912f7a89b06e98
GianmarcoSanzone <sanzonegianmarco@gmail.com>
2022-10-10 00:44:23 -0400

    Enemy gun and damage noises

--- Assets/Scripts/enemyAi.cs
+++ Assets/Scripts/enemyAi.cs
@@ -9,6 +9,9 @@ public class enemyAi : MonoBehaviour, IDamage
 	[SerializeField] NavMeshAgent agent;
 	[SerializeField] Renderer model;
 	[SerializeField] private Animator animator;
+	[SerializeField] public AudioSource gunShot;
+	[SerializeField] public AudioSource footSteps;
+	[SerializeField] public AudioSource grunt;
 
 	[Header("-----Enemy Stats-----")]
 	public float maxHealth = 10;
@@ -25,13 +28,14 @@ public class enemyAi : MonoBehaviour, IDamage
 	
 	 bool InRadius;
 	bool isShooting;
-	
-	
-	
-	
+
+
+
 	// Start is called before the first frame update
 	void Start()
 	{
+		grunt.pitch = 2;
+		grunt.volume = .598f;
 		GameManager.instance.enemyNumber++;
 		currentHealth = maxHealth;
 		GameManager.instance.enemyCountText.text = GameManager.instance.enemyNumber.ToString("F0");
@@ -44,8 +48,10 @@ public class enemyAi : MonoBehaviour, IDamage
 		
 		if (InRadius)
 		{
-			Aggro();
 			
+			Aggro();
+
+
 			if (!isShooting)
 			{
 				StartCoroutine(Shoot());
@@ -60,10 +66,15 @@ public class enemyAi : MonoBehaviour, IDamage
 				}
 			}
             else
-            {
+			{
 				animator.SetInteger("Status_walk", 1);
 			}
 
+			
+		}
+        else
+        {
+			animator.SetInteger("Status_walk", 0);
 		}
 
 	}
@@ -73,11 +84,9 @@ public class enemyAi : MonoBehaviour, IDamage
 	{
         if (agent.enabled)
         {
-	        agent.SetDestination(GameManager.instance.player.transform.position);
-	        
-        }
-		
-		
+			
+			agent.SetDestination(GameManager.instance.player.transform.position);
+		}
 		//Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
 		//rig.MovePosition(pos);
 		//transform.LookAt(target);
@@ -91,12 +100,11 @@ public class enemyAi : MonoBehaviour, IDamage
 	{
 		currentHealth -= dmg;
 		enemyHpBar.fillAmount = currentHealth/maxHealth;
+		grunt.Play(1);
 		StartCoroutine(flashDamage());
 		if (currentHealth <= 0)
 		{
-			
-			Destroy(gameObject);
-			GameManager.instance.CheckEnemyTotal();
+			StartCoroutine(death());
 		}
 	}
 
@@ -115,9 +123,12 @@ public class enemyAi : MonoBehaviour, IDamage
 		isShooting = true;
 		
 		Instantiate(bullet, shotPoint.transform.position, transform.rotation);
-		
+
+		gunShot.Play();
+
 		yield return new WaitForSeconds(shootRate);
-		
+
+		gunShot.Stop();
 		isShooting = false;
 	}
 
@@ -141,7 +152,14 @@ public class enemyAi : MonoBehaviour, IDamage
 			InRadius = false;
 		}
 	}
-	
-	
 
+	IEnumerator death()
+    {
+		grunt.pitch = 1;
+		grunt.volume = 1;
+		grunt.Play(1);
+		yield return new WaitForSeconds(.32f);
+		Destroy(gameObject);
+		GameManager.instance.CheckEnemyTotal();
+	}
 }
