826f9d2e432930409e9bd0cb3e58f8d0b7cab6a6
NotDom979 <blkwhte979@gmail.com>
2022-10-11 03:55:13 -0400

    published

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
 	public void PlayGame()
 	{
-		SceneManager.LoadScene("FirstScene");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	public void QuitGame()
 	{
