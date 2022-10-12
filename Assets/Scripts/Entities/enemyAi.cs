0dfe48f4f35cc5530576f0ad733e59ec9b464dca
GianmarcoSanzone <sanzonegianmarco@gmail.com>
2022-10-11 23:23:44 -0400

    Updated enemy AI + Roaming

--- Assets/Scripts/Entities/enemyAi.cs
+++ Assets/Scripts/Entities/enemyAi.cs
@@ -5,167 +5,209 @@ using UnityEngine.AI;
 using UnityEngine.UI;
 public class enemyAi : MonoBehaviour, IDamage
 {
-	[Header("-----Components-----")]
-	[SerializeField] NavMeshAgent agent;
-	[SerializeField] Renderer model;
-	[SerializeField] private Animator animator;
-	[SerializeField] public AudioSource gunShot;
-	[SerializeField] public AudioSource footSteps;
-	[SerializeField] public AudioSource grunt;
+    [Header("-----Components-----")]
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] private Animator animator;
+    [SerializeField] public AudioSource gunShot;
+    [SerializeField] public AudioSource footSteps;
+    [SerializeField] public AudioSource grunt;
 
-	[Header("-----Enemy Stats-----")]
-	public float maxHealth = 10;
-	float currentHealth;
-	public Image enemyHpBar;
-	[SerializeField] int sightDistance;
+    [Header("-----Enemy Stats-----")]
+    public float maxHealth = 10;
+    float currentHealth;
+    public Image enemyHpBar;
+    [SerializeField] int sightDistance;
+    [SerializeField] int roamDist;
+    [SerializeField] int viewAngle;
+    [SerializeField] GameObject HeadPos;
+    [SerializeField] int speedChase;
+    [SerializeField] int FacePlayerSpeed;
 
-	[Header("-----Enemy Gun Stats-----")]
-	[SerializeField] float shootRate;
-	[SerializeField] GameObject bullet;
-	[SerializeField] GameObject shotPoint;
-	
-	
-	
-	 bool InRadius;
-	bool isShooting;
-
-
-
-	// Start is called before the first frame update
-	void Start()
-	{
-		grunt.pitch = 2;
-		grunt.volume = .598f;
-		GameManager.instance.enemyNumber++;
-		currentHealth = maxHealth;
-		GameManager.instance.enemyCountText.text = GameManager.instance.enemyNumber.ToString("F0");
-		//healthBar.SetMaxHealth(maxHealth);
-	}
+    [Header("-----Enemy Gun Stats-----")]
+    [SerializeField] float shootRate;
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject shotPoint;
 
 
-	void Update()
-	{
-		
-		if (InRadius)
-		{
-			
-			Aggro();
-			animator.SetInteger("Status_walk", 1);
-			footSteps.enabled = true;
-			if (!isShooting)
-			{
-				StartCoroutine(Shoot());
-			}
 
-			if (agent.stoppingDistance > agent.remainingDistance)
-			{
-				RaycastHit hit;
-				Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit);
-				footSteps.enabled = false;
-				animator.enabled = false;
-				if (GameManager.instance.playerScript.controller.isGrounded)
-				{
-					gameObject.transform.LookAt(new Vector3(GameManager.instance.player.transform.position.x, 0, GameManager.instance.player.transform.position.z ));
-				}
-			}
-            else
-			{
-	            animator.enabled = true;
-	            footSteps.enabled = true;
-				animator.SetInteger("Status_walk", 1);
-			}
+    bool InRadius;
+    bool isShooting;
 
-			
-		}
-        else
-		{
-        	footSteps.enabled = false;
-			animator.SetInteger("Status_walk", 0);
-		}
+    Vector3 playerDirection;
+    float stoppingDistOrigin;
+    Vector3 startPos;
+    float angle;
+    float speedPatrol;
 
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        grunt.pitch = 2;
+        grunt.volume = .598f;
+        GameManager.instance.enemyNumber++;
+        currentHealth = maxHealth;
+        GameManager.instance.enemyCountText.text = GameManager.instance.enemyNumber.ToString("F0");
+        //healthBar.SetMaxHealth(maxHealth);
+
+        stoppingDistOrigin = agent.stoppingDistance;
+        agent.stoppingDistance = 0;
+
+        startPos = transform.position;
+
+        speedPatrol = agent.speed;
+        animator.SetInteger("Status_walk", 1);
+        Roam();
+    }
 
 
-	void Aggro()
-	{
+    void Update()
+    {
         if (agent.enabled)
         {
-			
-			agent.SetDestination(GameManager.instance.player.transform.position);
-		}
-		//Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
-		//rig.MovePosition(pos);
-		//transform.LookAt(target);
-	}
+                animator.SetInteger("Status_walk", 1);
+                footSteps.enabled = true;
+            if (InRadius)
+            {
+                playerDirection = GameManager.instance.player.transform.position - HeadPos.transform.position;
+                angle = Vector3.Angle(playerDirection, transform.forward);
 
+                CanSeePlayer();
 
+            }
+            else if (agent.remainingDistance < 0.1f && agent.destination != GameManager.instance.player.transform.position)
+            {
+                Roam();
+            }
+        }
+    }
 
-
-
-	public void takeDamage(float dmg)
-	{
-		currentHealth -= dmg;
-		enemyHpBar.fillAmount = currentHealth/maxHealth;
-		grunt.Play(1);
-		StartCoroutine(flashDamage());
-		if (currentHealth <= 0)
-		{
-			StartCoroutine(death());
-		}
-	}
-
-	IEnumerator flashDamage()
-	{
-		model.material.color = Color.red;
-		agent.enabled = false;
-		yield return new WaitForSeconds(.01f);
-		model.material.color = Color.white;
-		agent.enabled = true;
-
-	}
-	
-	IEnumerator Shoot()
-	{
-		isShooting = true;
-		
-		Instantiate(bullet, shotPoint.transform.position, transform.rotation);
-
-		gunShot.Play();
-
-		yield return new WaitForSeconds(shootRate);
-
-		gunShot.Stop();
-		isShooting = false;
-	}
-
-
-
-	private void OnTriggerEnter(Collider other)
-	{
-		
-		if (other.CompareTag("Player"))
-		{
-			//animator.SetInteger("Status_walk", 1);
-			InRadius = true;
-		}
-	}
-
-	private void OnTriggerExit(Collider other)
-	{
-		if (other.CompareTag("Player"))
-		{
-			//animator.SetInteger("Status_walk", 0);
-			InRadius = false;
-		}
-	}
-
-	IEnumerator death()
+    public void takeDamage(float dmg)
     {
-		grunt.pitch = 1;
-		grunt.volume = 1;
-		grunt.Play(1);
-		yield return new WaitForSeconds(.32f);
-		Destroy(gameObject);
-		GameManager.instance.CheckEnemyTotal();
-	}
+        currentHealth -= dmg;
+        enemyHpBar.fillAmount = currentHealth / maxHealth;
+        grunt.Play(1);
+        StartCoroutine(flashDamage());
+        if (currentHealth <= 0)
+        {
+            StartCoroutine(death());
+        }
+    }
+
+    IEnumerator flashDamage()
+    {
+        model.material.color = Color.red;
+        agent.enabled = false;
+        yield return new WaitForSeconds(.01f);
+        model.material.color = Color.white;
+        agent.enabled = true;
+        agent.stoppingDistance = 0;
+        agent.SetDestination(GameManager.instance.player.transform.position);
+    }
+
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        Instantiate(bullet, shotPoint.transform.position, transform.rotation);
+
+        gunShot.Play();
+
+        yield return new WaitForSeconds(shootRate);
+
+        gunShot.Stop();
+        isShooting = false;
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            //animator.SetInteger("Status_walk", 1);
+            InRadius = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //animator.SetInteger("Status_walk", 0);
+            InRadius = false;
+            agent.stoppingDistance = 0;
+
+        }
+    }
+
+    IEnumerator death()
+    {
+        grunt.pitch = 1;
+        grunt.volume = 1;
+        grunt.Play(1);
+        yield return new WaitForSeconds(.32f);
+        Destroy(gameObject);
+        GameManager.instance.CheckEnemyTotal();
+    }
+
+    void CanSeePlayer()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(HeadPos.transform.position, playerDirection, out hit, sightDistance))
+        {
+            Debug.DrawRay(HeadPos.transform.position, playerDirection);
+            Debug.Log(angle);
+            if (hit.collider.CompareTag("Player") && angle <= viewAngle)
+            {
+                agent.speed = speedChase;
+                agent.stoppingDistance = stoppingDistOrigin;
+                agent.SetDestination(GameManager.instance.player.transform.position);
+
+                if (!isShooting)
+                {
+                    StartCoroutine(Shoot());
+                }
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    facePlayer();
+                }
+            }
+
+        }
+
+
+    }
+
+
+    void facePlayer()
+    {
+        playerDirection.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(playerDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * FacePlayerSpeed);
+    }
+
+
+    void Roam()
+    {
+        agent.stoppingDistance = 0;
+        agent.speed = speedPatrol;
+        Vector3 randomDir = Random.insideUnitSphere * roamDist;
+
+        randomDir += startPos;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDir, out hit, 1, 1);
+        NavMeshPath path = new NavMeshPath();
+
+        agent.CalculatePath(hit.position, path);
+
+
+        agent.SetPath(path);
+
+    }
+
 }
