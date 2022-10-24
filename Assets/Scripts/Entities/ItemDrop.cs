29cb6e5fd9e2fd8a65ddb543f839475728bdd0eb
GameDev-Ed <edwardspellman.swe@gmail.com>
2022-10-24 15:29:21 -0400

    f

---
+++ Assets/Scripts/Entities/ItemDrop.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour
+{
+    [SerializeField] GameObject[] itemsDrops;
+   [Range(0,4)] [SerializeField] public float randItem;
+    [SerializeField]
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
