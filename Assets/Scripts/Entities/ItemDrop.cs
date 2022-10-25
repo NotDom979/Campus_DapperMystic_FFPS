6ff8dfb80343a17d9399581f5304ceef52a58269
Edward Spellman <edwardspellman.swe@gmail.com>
2022-10-24 21:21:18 -0400

    in the works don't merge it

--- Assets/Scripts/Entities/ItemDrop.cs
+++ Assets/Scripts/Entities/ItemDrop.cs
@@ -7,38 +7,46 @@ public class ItemDrop : MonoBehaviour
     [SerializeField] GameObject[] itemsDrops;
     [SerializeField] public int randItem;
     [SerializeField] public int grabItem;
-    [SerializeField] Transform deathPos;
+   
     enemyAi enemy;
-    void Start()
+    private void Start()
     {
-        deathPos = GetComponent<Transform>();
-
+   
+        Debug.Log(itemsDrops);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RandomItem();
     }
 
-    public void Randomitem() 
+    void RandomItem()
     {
-
-        if (enemy.maxHealth <= 0 )
+        if (enemy.maxHealth <= 0)
         {
+            randItem = Random.Range(0, 6);
             if (randItem == 2)
             {
                 grabItem = 2;
-                Instantiate(itemsDrops[1], deathPos.position, Quaternion.identity);
+                Instantiate(itemsDrops[grabItem], transform.position, Quaternion.identity);
 
             }
-            else if (randItem == 5)
+            else if (randItem == 1)
             {
-                grabItem = 5;
+                grabItem = 1;
+                Instantiate(itemsDrops[grabItem], transform.position, Quaternion.identity);
+
+            }
+            else if (randItem == 3)
+            {
+
+                grabItem = 3;
+                Instantiate(itemsDrops[grabItem], transform.position, Quaternion.identity);
             }
         }
-    
-    
+
+
     }
 
 
