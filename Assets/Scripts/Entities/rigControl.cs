7135dc043a94ef40c10b03d38bb5066baf8c2f9c
NotDom979 <blkwhte979@gmail.com>
2022-10-26 14:57:02 -0400

    playerArms move with character

--- Assets/Scripts/Entities/rigControl.cs
+++ Assets/Scripts/Entities/rigControl.cs
@@ -4,38 +4,15 @@ using UnityEngine;
 
 public class rigControl : MonoBehaviour
 {
-	[SerializeField] int sensHort;
-	[SerializeField] int sensVert;
-	[SerializeField] int lockVertMin;
-	[SerializeField] int lockVertMax;
-	[SerializeField] bool invert;
-	float xRotation;
 	// Start is called before the first frame update
 	void Start()
 	{
-		Cursor.lockState =CursorLockMode.Locked;
-		Cursor.visible = false;
 	}
 
 	// Update is called once per frame
-	void LateUpdate()
+	void Update()
 	{
-		//get input 
-		float mousex = Input.GetAxis("Mouse Y") * Time.deltaTime * sensHort;
-		float mousey = Input.GetAxis("Mouse X") * Time.deltaTime * sensVert;
-	    
-		if(invert)
-			xRotation += mousey;
-		else
-			xRotation -= mousey;
-		//clamp camera rotation
-		xRotation = Mathf.Clamp(xRotation,lockVertMin,lockVertMax);
-	    
-		//rotate camera on x axis
-		transform.localRotation = Quaternion.Euler(xRotation,0,0);
-	    
-		//rotate the player
-		transform.parent.Rotate(Vector3.up * mousex);
-	    
+		Vector3 mousePos = Input.mousePosition;
+		transform.LookAt(Camera.main.ScreenToWorldPoint( new Vector3(mousePos.x, mousePos.y, 10)));
 	}
 }
