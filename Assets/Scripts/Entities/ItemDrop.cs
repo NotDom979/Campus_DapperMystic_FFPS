9e4bfb51b9927063ce10e410158ca3efaf65302c
Edward Spellman <edwardspellman.swe@gmail.com>
2022-10-26 02:39:56 -0400

    look into it more in class

--- Assets/Scripts/Entities/ItemDrop.cs
+++ Assets/Scripts/Entities/ItemDrop.cs
@@ -6,39 +6,41 @@ public class ItemDrop : MonoBehaviour
 {
     [SerializeField] GameObject[] itemsDrops;
     [SerializeField] public int randItem;
-    [SerializeField] public int grabItem;
+    private int grabItem;
     public GameObject enemy;
 
     private void Start()
     {
-        enemy = GameObject.FindGameObjectWithTag(gameObject.tag);
+             randItem = Random.Range(0, 5);
+        enemy = GameObject.FindGameObjectWithTag("enemy");
         Debug.Log(itemsDrops);
     }
 
     // Update is called once per frame
     void Update()
     {
-        RandomItem();
 
+        RandomItem();
     }
 
     public void RandomItem()
     {
+ 
         
-             randItem = Random.Range(0, 5);
+
 
             if (randItem == 2)
             {
                 grabItem = 2;
                 Instantiate(itemsDrops[grabItem], transform.position, Quaternion.identity);
-                Destroy(gameObject);
+             
                 
             }
             else if (randItem == 1)
             {
                 grabItem = 1;
                 Instantiate(itemsDrops[grabItem], transform.position, Quaternion.identity);
-                Destroy(gameObject);
+               
 
             }
             else if (randItem == 3)
@@ -46,12 +48,13 @@ public class ItemDrop : MonoBehaviour
 
                 grabItem = 3;
                 Instantiate(itemsDrops[grabItem], transform.position, Quaternion.identity);
-                Destroy(gameObject);
+                
 
             }
         
         
     }
+        
 
 
 
