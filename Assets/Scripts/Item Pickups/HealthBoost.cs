166132584d1f175d5b6eaa9327c772a9f7415c0d
NotDom979 <blkwhte979@gmail.com>
2022-10-27 17:27:05 -0400

    Health Boost Work/ Damage Boost Wip

---
+++ Assets/Scripts/Item Pickups/HealthBoost.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBoost : MonoBehaviour
+{
+	int HP;
+	playerController player;
+	
+	void Start()
+	{
+		HP = GameManager.instance.playerScript.HP;
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		player = other.GetComponent<playerController>();
+		if (other.CompareTag("Player"))
+		{
+			player.HP *= 2;
+			Destroy(gameObject);
+			StartCoroutine(Wait());
+			
+			
+		}
+	}
+	IEnumerator Wait(){
+		yield return new WaitForSeconds(30f);
+		player.HP = HP;
+	}
+	
+}
