42935f393a47d6bed03c57c12777d7975321cb4a
NotDom979 <blkwhte979@gmail.com>
2022-10-28 02:13:03 -0400

    Basic Audio settings done/ needs value tracking

---
+++ Assets/Scripts/UI/Volume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+public class Volume: MonoBehaviour
+{
+	public AudioMixer audioMixer;
+	public void MasterVolume(float volume)
+	{
+		audioMixer.SetFloat("MasterVol",volume);
+		
+	}
+	public void SFXVolume(float volume)
+	{
+		audioMixer.SetFloat("SFXVol",volume);
+	}
+	public void MusicVolume(float volume)
+	{
+		audioMixer.SetFloat("MusicVol",volume);
+	}
+}
---
+++ Assets/Scripts/UI/VolumeText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+public class VolumeText : MonoBehaviour
+{
+	public TextMeshProUGUI MasterVol;
+	public TextMeshProUGUI MusicVol;
+	public TextMeshProUGUI SFXVol;
+	public AudioMixer audioMix;
+	
+    // Start is called before the first frame update
+    void Start()
+	{
+    	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    MasterVol.text = audioMix.GetFloat("MasterVol").ToString("F0");
+	    SFXVol.text = audioMix.GetFloat("SFXVol").ToString("F0");
+	    MusicVol.text = audioMix.GetFloat("MusicVol").ToString("F0");
+    }
+}
