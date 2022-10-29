a3365e15d88cbedd62a396c7e328ac9c83e075dd
Edward Spellman <edwardspellman.swe@gmail.com>
2022-10-28 22:13:51 -0400

    made armor boost

---
+++ Assets/Scripts/Item Pickups/ArmorBoost.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorBoost : MonoBehaviour
+{
+	int armor;
+	playerController player;
+	
+	void Start()
+	{
+		armor = GameManager.instance.playerScript.Armor;
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		player = other.GetComponent<playerController>();
+		if (other.CompareTag("Player"))
+		{
+			player.Armor *= 2;
+			Destroy(gameObject);
+			StartCoroutine(Wait());
+			
+			
+		}
+	}
+	IEnumerator Wait(){
+		yield return new WaitForSeconds(30f);
+		player.Armor = armor;
+	}
+	
+}
