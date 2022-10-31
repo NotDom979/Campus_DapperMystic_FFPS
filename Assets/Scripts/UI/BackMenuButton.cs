d16b70642997365ba13b2a36f05d5dc6e8a78459
NotDom979 <blkwhte979@gmail.com>
2022-10-31 11:48:20 -0400

    New Options menu up and running

---
+++ Assets/Scripts/UI/BackMenuButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackMenuButton : MonoBehaviour
+{
+	public void BackButton()
+	{
+		
+		GameManager.instance.pauseMenu.SetActive(true);
+		GameManager.instance.optionMenu.SetActive(false);
+	}
+	public void MenuButton()
+	{
+		GameManager.instance.pauseMenu.SetActive(false);
+		SceneManager.LoadScene("Menu");
+	}
+	public void OptionButton()
+	{
+		GameManager.instance.pauseMenu.SetActive(false);
+		GameManager.instance.optionMenu.SetActive(true);
+	}
+}
