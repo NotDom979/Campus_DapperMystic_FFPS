ca4882387afedb1aff3a990fd40c72a6eec5551a
GameDev-Ed <edwardspellman.swe@gmail.com>
2022-10-31 10:21:55 -0400

    don't know what to call this commit

--- Assets/Scripts/Entities/enemyAi.cs
+++ Assets/Scripts/Entities/enemyAi.cs
@@ -11,9 +11,9 @@ public class enemyAi : MonoBehaviour, IDamage
     [SerializeField] private Animator animator;
     [SerializeField] public AudioSource gunShot;
     [SerializeField] public AudioSource footSteps;
-	[SerializeField] public AudioSource grunt;
-	[SerializeField] public GameObject EnemyCanvas;
-	[SerializeField] public GameObject Detector;
+    [SerializeField] public AudioSource grunt;
+    [SerializeField] public GameObject EnemyCanvas;
+    [SerializeField] public GameObject Detector;
 
     [Header("-----Enemy Stats-----")]
     public float maxHealth = 10;
@@ -24,90 +24,101 @@ public class enemyAi : MonoBehaviour, IDamage
     [SerializeField] int viewAngle;
     [SerializeField] GameObject HeadPos;
     [SerializeField] int speedChase;
-	[SerializeField] int FacePlayerSpeed;
-	[SerializeField] public GameObject muzzleFlash;
-	[SerializeField] public ParticleSystem muzzle;
-	ParticleSystem mf;
-	
+    [SerializeField] int FacePlayerSpeed;
+    [SerializeField] public GameObject muzzleFlash;
+    [SerializeField] public ParticleSystem muzzle;
+    ParticleSystem mf;
+
     [Header("-----Enemy Gun Stats-----")]
     [SerializeField] float shootRate;
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject shotPoint;
 
+
+    [Header("-----Item Drop-----")]
+    [SerializeField] GameObject[] itemsDrops;
+    [SerializeField] public int randItem;
+    private int grabItem;
+
+
     public bool flamer;
 
-	public bool InRadius;
+    public bool InRadius;
     bool isShooting;
-	Vector3 playerDirection;
+    Vector3 playerDirection;
     float stoppingDistOrigin;
     Vector3 startPos;
     float angle;
-	float speedPatrol;
-	bool playerSeen;
+    float speedPatrol;
+    bool playerSeen;
     // Start is called before the first frame update
     void Start()
-	{
-		EnemyCanvas = GameObject.FindGameObjectWithTag("EnemyCanvas");
-	    footSteps.enabled = false;
+    {
+        EnemyCanvas = GameObject.FindGameObjectWithTag("EnemyCanvas");
+        footSteps.enabled = false;
         gunShot.enabled = false;
         grunt.pitch = 2;
         grunt.volume = .598f;
-	    GameManager.instance.enemyNumber++;
+        GameManager.instance.enemyNumber++;
         currentHealth = maxHealth;
         GameManager.instance.enemyCountText.text = GameManager.instance.enemyNumber.ToString("F0");
-	    playerSeen = false;
+        playerSeen = false;
         stoppingDistOrigin = agent.stoppingDistance;
         agent.stoppingDistance = 0;
-		
+
         startPos = transform.position;
 
         speedPatrol = agent.speed;
-	    // animator.SetInteger("Status_walk", 1);
+        // animator.SetInteger("Status_walk", 1);
         Roam();
     }
 
 
     void Update()
-	{
-		if (GameManager.instance.pauseMenu.activeSelf == false)
-		{
-			Detection();
-			if (agent.enabled)
-			{
-				//animator.SetInteger("Status_walk", 1);
-				animator.SetFloat("Speed", Mathf.Lerp(animator.GetFloat("Speed"), agent.velocity.normalized.magnitude, Time.deltaTime * 3));
-				footSteps.enabled = true;
-				if (InRadius)
-				{
-					footSteps.enabled = true;
-					playerDirection = GameManager.instance.player.transform.position - HeadPos.transform.position;
-					angle = Vector3.Angle(playerDirection, transform.forward);
+    {
+        if (GameManager.instance.pauseMenu.activeSelf == false)
+        {
+            Detection();
+            if (agent.enabled)
+            {
+                //animator.SetInteger("Status_walk", 1);
+                animator.SetFloat("Speed", Mathf.Lerp(animator.GetFloat("Speed"), agent.velocity.normalized.magnitude, Time.deltaTime * 3));
+                footSteps.enabled = true;
+                if (InRadius)
+                {
+                    footSteps.enabled = true;
+                    playerDirection = GameManager.instance.player.transform.position - HeadPos.transform.position;
+                    angle = Vector3.Angle(playerDirection, transform.forward);
 
-					CanSeePlayer();
+                    CanSeePlayer();
 
-				}
-				if (playerSeen == true)
-				{
-					facePlayer();
-				}
-				else if (agent.remainingDistance < 0.1f && agent.destination != GameManager.instance.player.transform.position)
-				{
-					Roam();
-					animator.Play("Idle");
-				}
-				else
-				{
-					animator.Play("Idle");
-					gunShot.enabled = false;
-				}
-			}
-		}
-		else
-		{
-			footSteps.enabled = false;
-		}
+                }
+                if (playerSeen == true)
+                {
+                    facePlayer();
+                }
+                else if (agent.remainingDistance < 0.1f && agent.destination != GameManager.instance.player.transform.position)
+                {
+                    Roam();
+                    animator.Play("Idle");
+                }
+                else
+                {
+                    animator.Play("Idle");
+                    gunShot.enabled = false;
+                }
+            }
+        }
+        else
+        {
+            footSteps.enabled = false;
+        }
     }
 
+    public void payDay(int currency)
+    {
+
+    }
     public void takeDamage(float dmg)
     {
         currentHealth -= dmg;
@@ -118,10 +129,10 @@ public class enemyAi : MonoBehaviour, IDamage
             StartCoroutine(death());
         }
         else
-	        gameObject.GetComponent<Animator>().Play("Hit");
-            
-	    animator.SetBool("Hit",true);
-	    agent.SetDestination(GameManager.instance.player.transform.position);
+            gameObject.GetComponent<Animator>().Play("Hit");
+
+        animator.SetBool("Hit", true);
+        agent.SetDestination(GameManager.instance.player.transform.position);
         StartCoroutine(flashDamage());
     }
 
@@ -132,18 +143,18 @@ public class enemyAi : MonoBehaviour, IDamage
         yield return new WaitForSeconds(.5f);
         model.material.color = Color.white;
         agent.speed = speedPatrol;
-	    agent.stoppingDistance = 0;
-	    animator.SetBool("Hit",false);
+        agent.stoppingDistance = 0;
+        animator.SetBool("Hit", false);
     }
 
     IEnumerator Shoot()
     {
         isShooting = true;
-	    animator.Play("Shoot");
+        animator.Play("Shoot");
         yield return new WaitForSeconds(.25f);
-	    Instantiate(bullet, shotPoint.transform.position, transform.rotation);
-	    gunShot.enabled = true;
-	    Muzzle();
+        Instantiate(bullet, shotPoint.transform.position, transform.rotation);
+        gunShot.enabled = true;
+        Muzzle();
         if (flamer)
         {
             gunShot.loop = true;
@@ -157,28 +168,28 @@ public class enemyAi : MonoBehaviour, IDamage
             gunShot.Stop();
         }
 
-	    isShooting = false;
-	    
+        isShooting = false;
+
     }
 
-	private void Detection()
-	{
-		if (gameObject.GetComponentInChildren<DetectionRadius>().inRadius == true)
-		{
-			InRadius = true;
-			facePlayer();
-		}
-		else
-		{
-			InRadius = false;
-			agent.stoppingDistance = 0;
-		}
-	}
+    private void Detection()
+    {
+        if (gameObject.GetComponentInChildren<DetectionRadius>().inRadius == true)
+        {
+            InRadius = true;
+            facePlayer();
+        }
+        else
+        {
+            InRadius = false;
+            agent.stoppingDistance = 0;
+        }
+    }
     IEnumerator death()
     {
-	    animator.Play("Dead");
-	    animator.SetBool("Dead", true);
-	    EnemyCanvas.SetActive(false);
+        animator.Play("Dead");
+        animator.SetBool("Dead", true);
+        EnemyCanvas.SetActive(false);
         agent.speed = 0;
         grunt.pitch = 1;
         grunt.Play(1);
@@ -188,8 +199,9 @@ public class enemyAi : MonoBehaviour, IDamage
             yield return new WaitForSeconds(3);
         }
         else
-	        agent.enabled = false;
-	        yield return new WaitForSeconds(2f);
+            RandomItem();
+            agent.enabled = false;
+        yield return new WaitForSeconds(2f);
         Destroy(gameObject);
         GameManager.instance.CheckEnemyTotal();
     }
@@ -198,13 +210,13 @@ public class enemyAi : MonoBehaviour, IDamage
     {
         RaycastHit hit;
 
-	    if (Physics.Raycast(HeadPos.transform.position, playerDirection, out hit, sightDistance))
+        if (Physics.Raycast(HeadPos.transform.position, playerDirection, out hit, sightDistance))
         {
             Debug.DrawRay(HeadPos.transform.position, playerDirection);
             Debug.Log(angle);
-		    if (hit.collider.CompareTag("Player"))
-		    {
-			    playerSeen = true;
+            if (hit.collider.CompareTag("Player"))
+            {
+                playerSeen = true;
                 if (angle <= viewAngle)
                 {
 
@@ -230,7 +242,7 @@ public class enemyAi : MonoBehaviour, IDamage
             }
             else
             {
-            	agent.stoppingDistance = 0;
+                agent.stoppingDistance = 0;
                 gunShot.Stop();
             }
 
@@ -241,7 +253,7 @@ public class enemyAi : MonoBehaviour, IDamage
     }
 
 
-	public void facePlayer()
+    public void facePlayer()
     {
         playerDirection.y = 0;
         Quaternion rotation = Quaternion.LookRotation(playerDirection);
@@ -250,40 +262,65 @@ public class enemyAi : MonoBehaviour, IDamage
 
 
     void Roam()
-	{
-		//animator.SetInteger("Status_walk", 1);
+    {
+        //animator.SetInteger("Status_walk", 1);
 
-		agent.stoppingDistance = 0;
-		agent.speed = speedPatrol;
-        
+        agent.stoppingDistance = 0;
+        agent.speed = speedPatrol;
+
         Vector3 randomDir = Random.insideUnitSphere * roamDist;
         randomDir += startPos;
 
         NavMeshHit hit;
-		NavMesh.SamplePosition(randomDir, out hit, .5f, 1);
+        NavMesh.SamplePosition(randomDir, out hit, .5f, 1);
         NavMeshPath path = new NavMeshPath();
         if (hit.hit == true)
         {
-		    if (hit.position != null)
-		    {
-			    agent.CalculatePath(hit.position, path);
-		    }
+            if (hit.position != null)
+            {
+                agent.CalculatePath(hit.position, path);
+            }
         }
         agent.SetPath(path);
 
-	}
-	void Muzzle()
-	{
-		mf = Instantiate(muzzle, shotPoint.transform.position, transform.rotation);
-		muzzleFlash.SetActive(true);
-		mf.Play();
-		StartCoroutine(Wait());
-		muzzleFlash.SetActive(false);
-		mf.Stop();
-	}
-	IEnumerator Wait()
-	{
-		yield return new WaitForSeconds(.6f);
-	}
+    }
+    void Muzzle()
+    {
+        mf = Instantiate(muzzle, shotPoint.transform.position, transform.rotation);
+        muzzleFlash.SetActive(true);
+        mf.Play();
+        StartCoroutine(Wait());
+        muzzleFlash.SetActive(false);
+        mf.Stop();
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(.6f);
+    }
+    public void RandomItem()
+    {
+        randItem = Random.Range(0, 4);
 
+
+        if (randItem == 2)
+        {
+
+            Instantiate(itemsDrops[2], transform.position, Quaternion.identity);
+        }
+        else if (randItem == 1)
+        {
+            Instantiate(itemsDrops[1], transform.position, Quaternion.identity);
+
+
+        }
+        else if (randItem == 3)
+        {
+
+            Instantiate(itemsDrops[3], transform.position, Quaternion.identity);
+
+
+        }
+        Debug.Log(itemsDrops);
+    }
+  
 }
