561401181639fec4655b0801119e71c6005f7a0d
NotDom979 <blkwhte979@gmail.com>
2022-10-31 14:43:55 -0400

    OPtions

--- Assets/Scripts/UI/Volume.cs
+++ Assets/Scripts/UI/Volume.cs
@@ -13,21 +13,22 @@ public class Volume: MonoBehaviour
 	public void MasterVolume(float volume)
 	{
 		audioMixer.SetFloat("MasterVol",volume);
-		txt = (Mathf.Log10(slider.value) * 20);
+		txt = (Mathf.Pow(10.0f, volume / 20.0f) * 100);
 		Text.text = txt.ToString("F0");
 		
 	}
 	public void SFXVolume(float volume)
 	{
 		audioMixer.SetFloat("SFXVol",volume);
-		txt = (Mathf.Log10(slider.value) * 20);
+		txt = (Mathf.Pow(10.0f, volume / 20.0f) * 100);
 		Text.text = txt.ToString("F0");
 		
 	}
 	public void MusicVolume(float volume)
 	{
 		audioMixer.SetFloat("MusicVol",volume);
-		txt = (Mathf.Log10(slider.value) * 20);
+		txt = (Mathf.Pow(10.0f, volume / 20.0f) * 100);
+		
 		Text.text = txt.ToString("F0");
 		
 	}
