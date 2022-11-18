013ffcb7ceb9e8c7921f295b4925417b46261b92
NotDom979 <blkwhte979@gmail.com>
2022-11-18 14:59:57 -0500

    hotfix

---
+++ Assets/Scripts/Level/Credits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+	public Animator creditSlide;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    creditSlide.enabled = false;
+	    creditSlide.enabled = true;
+    }
+	void Update()
+	{
+		if(creditSlide.enabled == false)
+		{
+			creditSlide.enabled = true;
+		}
+	}
+
+}
